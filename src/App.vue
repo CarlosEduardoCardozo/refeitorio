db7dcd3150cb811316b691cb9d738bb96831a0d5
Leleguxta <leandrogustavosantos.info@gmail.com>
2022-10-19 11:10:01 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterView } from 'vue-router'
+import HeaderComp from './components/headerComp.vue';
+
+</script>
+
+<template>
+  <RouterView />
+  <HeaderComp />
+</template>
+
+<style></style>
